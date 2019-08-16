e52fcf6e238994534387c99a78992066482d76bd
Abiel Villarosa <john_abiel@yahoo.com>
2019-08-16 16:03:01 -0400

    Updates

---
+++ precache-manifest.ea6313b686a7d56f5a58a281458d6f7c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d6e3027299e157c0629a6d2ae0601f",
+    "url": "/strodApp/index.html"
+  },
+  {
+    "revision": "e454849c117854d4a399",
+    "url": "/strodApp/static/css/main.06b2e58a.chunk.css"
+  },
+  {
+    "revision": "ec006c78baf42e35c04f",
+    "url": "/strodApp/static/js/2.89154525.chunk.js"
+  },
+  {
+    "revision": "e454849c117854d4a399",
+    "url": "/strodApp/static/js/main.6cf70dac.chunk.js"
+  },
+  {
+    "revision": "afcf99679b11dbf2652a",
+    "url": "/strodApp/static/js/runtime~main.eebc4a20.js"
+  },
+  {
+    "revision": "c92ec00c4e4318afc9f42bf5fe9072f4",
+    "url": "/strodApp/static/media/bagel.c92ec00c.JPG"
+  },
+  {
+    "revision": "8674e3c5bce1ecbc7fe8fb466463b726",
+    "url": "/strodApp/static/media/coffee.8674e3c5.JPG"
+  },
+  {
+    "revision": "645a91a6c4bde0332865533da8da8c88",
+    "url": "/strodApp/static/media/donuts.645a91a6.JPG"
+  },
+  {
+    "revision": "aaf96547fc851bdb127cedd55ea28824",
+    "url": "/strodApp/static/media/pastries.aaf96547.JPG"
+  },
+  {
+    "revision": "5d3d115d10a6206389d2912c8bd805c2",
+    "url": "/strodApp/static/media/timbits.5d3d115d.JPG"
+  },
+  {
+    "revision": "d73bfa7bbd684b818b39d69c7122ac28",
+    "url": "/strodApp/static/media/wrap.d73bfa7b.JPG"
+  }
+]);
